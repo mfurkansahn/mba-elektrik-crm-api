64e876b9a9b22ad9879228c38f51ffd29aeddf45
Mustafa Furkan ŞAHİN <mfurkansahn@hotmail.com>
2026-07-28 16:58:28 +0300

    Add reference data endpoints

---
+++ Controllers/ReferenceDataController.cs
@@ -0,0 +1,24 @@
+﻿using MbaCrm.Api.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MbaCrm.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ReferenceDataController : ControllerBase
+    {
+        [HttpGet("service-request-statuses")]
+        public IActionResult GetServiceRequestStatuses()
+        {
+            return Ok(ServiceRequestStatuses.All);
+        }
+
+        [HttpGet("service-request-types")]
+        public IActionResult GetServiceRequestTypes()
+        {
+            return Ok(ServiceRequestTypes.All);
+        }
+    }
+}
